2f841824c5ebb4167050223ef8a61b334f4ce3aa
nope <no@reply.never>
2016-07-27 18:52:44 +0000

    more tweaks

--- compare.py
+++ compare.py
@@ -5,9 +5,11 @@ core = vs.get_core()
 SOURCE = 'source.mkv'
 ENCODE = 'encode.mkv'
 
+# Return the source's video frames
 def remux():
   return core.ffms2.Source(SOURCE)
-  
+
+# Return the encode's video frames
 def encode():
   return core.ffms2.Source(ENCODE)
 
@@ -17,78 +19,57 @@ def fill_border(clip, t=0, r=0, b=0, l=0):
 def crop(clip, t=0, r=0, b=0, l=0):
   return core.std.CropRel(clip=clip, top=t, right=r, bottom=b, left=l)
 
+# Select frames for testing
 def select_frames(clip):
   n = clip.num_frames
   clip_skipped = core.std.SelectEvery(clip, cycle = n, offsets = list(range(10000, n)))
   clip_new = core.std.SelectEvery(clip_skipped, cycle = 2000, offsets = list(range(0, 50)))
   return core.std.AssumeFPS(clip_new, clip)
 
+# Write Info to frames
 def write_text(clip, name):
   clip = core.text.Text(clip, text = name, alignment=1)
   clip = core.text.FrameNum(clip, alignment=8)
   return core.text.FrameProps(clip, ["_PictType"])
 
+# Original method by someusername
 def final_source_old():
   return select_frames(fill_border(crop(remux(), t=20, b=20), t=1, b=1))
 
-def final_source_new(top, bottom):
+# Return source with cropped black bars
+def SelectAndCropSource(top=0, bottom=0):
   return select_frames(fill_border(crop(remux(), t = top, b = bottom), t=1, b=1))
 
-def final_source_way_new(top, bottom):
-  return remux()
-  
-def compare():
-  clips = []
-  min = 16
-  max = 31
-
-  for i in range(min, max):
-    x = 15 + 0.2 * i
-
-    if i % 2 == 0:
-      r = final_source_old()
-      r = crop(r, t=2, b=2)
-      r = write_text(r, 'Remux')
-      clips.append(r)
-
-    clip = core.ffms2.Source(ENCODE.format(x))
-    clip = crop(clip, t=2, b=2)
-    clip = write_text(clip, '{0}.mkv'.format(x))
-    clips.append(clip)
-
-  if (max - min - 1) % 2 == 0:
-    r = final_source_old()
-    r = crop(r, t=2, b=2)
-    r = write_text(r, 'Remux')
-    clips.append(r)
-
-  return core.std.Interleave(clips, mismatch=True)
-
-def PreFinalRangeComparison():
+# Compare two sets of frames when testing
+# Arguments (both optional)
+#   croptop: crop black bars from top of source frame
+#   cropbottom: crop black bars from bottom of source frame
+def PreFinalRangeComparison(croptop=0, cropbottom=0):
   clips = []
   
-  r = final_source_new(0, 0)
-  #r = crop(r, t=2, b=2)
+  r = SelectAndCropSource(croptop, cropbottom)
   r = write_text(r, 'SOURCE')
   clips.append(r)
   
   clip = encode()
-  #clip = crop(clip, t=20, b=20)
   clip = write_text(clip, 'ENCODE @ crf18.5')
   clips.append(clip)
   
   return core.std.Interleave(clips, mismatch=True)
-  
-def FinalRangeComparison():
+
+# Compare two sets of frames after the final encode is done
+# Arguments (both optional)
+#   croptop: crop black bars from top of source frame
+#   cropbottom: crop black bars from bottom of source frame
+def FinalRangeComparison(croptop=0, cropbottom=0):
   clips = []
-  
-  r = final_source_way_new(0, 0)
-  #r = crop(r, t=2, b=2)
+
+  r = remux()
+  r = crop(r, t=croptop, b=cropbottom)
   r = write_text(r, 'SOURCE')
   clips.append(r)
   
   clip = encode()
-  #clip = crop(clip, t=20, b=20)
   clip = write_text(clip, 'ENCODE @ crf18.5')
   clips.append(clip)
   
